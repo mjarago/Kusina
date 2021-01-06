36f0af035029491d913a7530206ec8f60b412448
mjarago <markjosepharago@gmail.com>
2021-01-05 20:10:28 -0800

    typo in extended ingredient

--- app/src/main/java/com/markarago/kusina/models/ExtendedIngredient.kt
+++ app/src/main/java/com/markarago/kusina/models/ExtendedIngredient.kt
@@ -2,7 +2,7 @@ package com.markarago.kusina.models
 
 data class ExtendedIngredient(
     val amount: Double,
-    val consistenc: String,
+    val consistency: String,
     val image: String,
     val name: String,
     val original: String,
