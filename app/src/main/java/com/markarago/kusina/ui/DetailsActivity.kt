341171eb7b540c2cc83dad463e0fac6ea81e7ced
mjarago <markjosepharago@gmail.com>
2021-02-01 11:20:01 -0800

    Details Activity Design

---
+++ app/src/main/java/com/markarago/kusina/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.markarago.kusina.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.markarago.kusina.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
